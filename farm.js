b6c2950ca48fbb6f3a6b534f350f0735c4131478
Dominique Pendjol <d.pendjol@gmail.com>
2020-11-02 14:30:10 +0100

    Eindopdracht: TDD Groentetuin. Got everything setup, ready to go.

---
+++ farm.js
@@ -0,0 +1,20 @@
+//Kosten
+
+const get_yield_for_plant = () => {
+
+}
+
+const get_yield_for_crop = () => {
+
+}
+
+const get_total_yield = () => {
+    
+}
+
+
+module.exports = {
+    get_yield_for_plant,
+    get_yield_for_crop,
+    get_total_yield,
+  }
