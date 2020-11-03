e233aabbdf0b722b7420197e0b18cc071520bbd5
Dominique Pendjol <d.pendjol@gmail.com>
2020-11-03 11:20:26 +0100

    get_yield_for_plant --> wrote function to pass the test

--- farm.js
+++ farm.js
@@ -1,10 +1,46 @@
 
 // display value of yield property from plant
-const get_yield_for_plant = (plant) => plant.yield;
 
+const { getEnabledCategories } = require("trace_events");
+
+// to account for enviroment used spread operator to pass multiple parameters to object with out changing al the current calls to the function.
+const get_yield_for_plant = (...args) => {
+  const plant = args[0];
+  const num_amount_of_influence = [];
+  if (args.length > 1) {
+    const env_vars = args[1];
+
+    const env_vars_of_influence = Object.getOwnPropertyNames(env_vars);
+
+    // check if plant is susseptable to enviroment variables, other words, does the factor property exist
+    if (plant.factors !== undefined) {
+      // loop tru the factors
+      env_vars_of_influence.forEach(item => {
+        const text_amount_of_influence = env_vars[item];
+        // check if the environment variabele influences the plant
+        if (plant.factors[item] !== undefined) {
+          // if so store the percentage in a array
+          num_amount_of_influence.push(plant.factors[item][text_amount_of_influence]);
+        }
+      })
+    }
+  }
+  if (num_amount_of_influence.length !== null) {
+    const calculated_plant_yield = num_amount_of_influence.reduce((result, percentage) => {
+      return result * ((100 + percentage) / 100)
+    }, plant.yield);
+    return Math.round(calculated_plant_yield*10) / 10;
+  } else {
+   return plant.yield;
+  }
+}
 
 // display num_plants * yield per plant
-const get_yield_for_crop = (input) => input.num_plants * get_yield_for_plant(input.crop);
+const get_yield_for_crop = (input) => {
+  // console.log('Heeft die property factors: ', input[0].hasOwnProperty('yield'));
+  // console.log('alle object properties: ', Object.getOwnPropertyNames(input[0].crop))
+  return input.num_plants * get_yield_for_plant(input.crop);
+}
 
 // total the sum of all yields of all crops
 const get_total_yield = (input) => input.crops.reduce((total, item) => total + get_yield_for_crop(item),0)
